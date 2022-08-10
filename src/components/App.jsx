a6927b23f861853f92cd8644a01ea0425361339e
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-10 22:50:42 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,11 +11,10 @@ export const App = () => {
   return (
     <div
       style={{
-        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 30,
         color: '#010101'
       }}
     >
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,14 +3,12 @@ import css from './TransactionHistory.module.css';
 export const TransactionHistory = ({ items }) => {
     return (
         <table className={css.transactionHistory}>
-            <tbody className={css.info}>
-                <thead className={css.listHistory} >
+            <tbody className={css.info}>                
                     <tr className={css.wrap}>               
                     <th className={css.type}>Type</th>
                     <th className={css.amount}>Amount</th>
                     <th className={css.currency}>Currency</th>
-                    </tr>
-                </thead>
+                    </tr>                
                 {items.map(({ id, type, amount, currency }) => (
                     <tr className={css.wrap} key={id}>
                         <th className={css.type}>{type}</th>
